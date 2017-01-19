bd77609809fc361cf9385618afda69ec5a7dfb77
mikykeane <miguekeca@gmail.com>
2017-01-19 13:22:13 +0100

    Added Sensor extension

--- app/src/main/java/com/example/charlie/practicaandroid/MainActivity.java
+++ app/src/main/java/com/example/charlie/practicaandroid/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.charlie.practicaandroid;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    //TODO Casitodo
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
