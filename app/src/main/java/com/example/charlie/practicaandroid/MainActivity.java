43258ef662ad720b78fa64d3a7acfb3fa9766263
Carlos Paradela <charlesprofelds@gmail.com>
2017-01-23 01:15:08 +0100

    Corregido crash

--- app/src/main/java/com/example/charlie/practicaandroid/MainActivity.java
+++ app/src/main/java/com/example/charlie/practicaandroid/MainActivity.java
@@ -40,19 +40,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @Override
-    protected void onStart(){
+    protected void onResume() {
+        super.onResume(); //registro del listener
         x1=p1.getX();
         y1=p1.getY();
         x2=p2.getX();
         y2=p2.getY();
-
-    }
-
-    @Override
-    protected void onResume() {
-
-        super.onResume(); //registro del listener
         SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);  //se utilizará el acelerómetro como sensor
         if (sensors.size() > 0) {
