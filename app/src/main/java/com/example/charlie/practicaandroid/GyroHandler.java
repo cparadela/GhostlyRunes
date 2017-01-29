daba4c3a48dc15e8aafe3a709bdcc82b22179dcd
Carlos Paradela <charlesprofelds@gmail.com>
2017-01-29 03:32:20 +0100

    Added Find the ghost!

--- app/src/main/java/com/example/charlie/practicaandroid/GyroHandler.java
+++ app/src/main/java/com/example/charlie/practicaandroid/GyroHandler.java
@@ -6,6 +6,9 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+import android.view.Gravity;
+
+import java.text.DecimalFormat;
 
 /**
  * Created by Charlie 28/01/2017.
@@ -17,13 +20,27 @@ public class GyroHandler implements SensorEventListener {
     int maxcount=20;
 
 
+    float[] normalized_gravity;
+
     // Create a constant to convert nanoseconds to seconds.
     private static final float NS2S = 1.0f / 1000000000.0f;
     private final float[] deltaRotationVector = new float[4];
     private float timestamp;
     private final float EPSILON=  0.000001f;
 
+    boolean coords_set = false;
+    float[] basevector = new float[3];
+    float[] basevector2 = new float[3];
 
+    float fantasma=1.1f;
+    float error =0.2f;
+    long t0;
+
+
+    float time_not_vib=0;
+
+
+    boolean f = false;
 
     public float [] rotationCurrent = new float[9];
 
@@ -42,15 +59,18 @@ public class GyroHandler implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         // This timestep's delta rotation to be multiplied by the current rotation
         // after computing it from the gyro sample data.
+
+
         if (timestamp != 0) {
             final float dT = (event.timestamp - timestamp) * NS2S;
+            time_not_vib+=dT;
             // Axis of the rotation sample, not normalized yet.
             float axisX = event.values[0];
             float axisY = event.values[1];
             float axisZ = event.values[2];
 
             // Calculate the angular speed of the sample
-            float omegaMagnitude = (float) Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+            float omegaMagnitude = (float) Math.sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ);
 
             // Normalize the rotation vector if it's big enough to get the axis
             // (that is, EPSILON should represent your maximum allowable margin of error)
@@ -79,36 +99,60 @@ public class GyroHandler implements SensorEventListener {
         // User code should concatenate the delta rotation we computed with the current rotation
         // in order to get the updated rotation.
         //rotationCurrent = rotationCurrent * deltaRotationMatrix;
-        float [] tempMatrix = new float[9];
-        for (int i=0; i<3; i++ ) {
-            for (int j=0; j<3; j++ ) {
-                tempMatrix[i*3 + j]=0;
+        float[] tempMatrix = new float[9];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tempMatrix[i * 3 + j] = 0;
                 for (int z = 0; z < 3; z++) {
-                    tempMatrix[i*3 + j] += rotationCurrent[i*3+z]*deltaRotationMatrix[z*3+j];
+                    tempMatrix[i * 3 + j] += rotationCurrent[i * 3 + z] * deltaRotationMatrix[z * 3 + j];
 
                 }
             }
 
         }
 
-        rotationCurrent=tempMatrix;
-        count++;
-        if(count>maxcount){
-        Log.d("Rotation Current:", " "+rotationCurrent[0]+" "+rotationCurrent[1]+" "+rotationCurrent[2]);
-            count=0;
-        }
+        rotationCurrent = tempMatrix;
         float[] gyroscopeOrientation = new float[3];
         SensorManager.getOrientation(rotationCurrent,
                 gyroscopeOrientation);
-        /*
 
-        Log.d( "Orientacion","\n\nOrientation X (Roll) :" + (new DecimalFormat("#.##").format((double)gyroscopeOrientation[0]))
-                + "\n\n" + "Orientation Y (Pitch) :"
-                + (new DecimalFormat("#.##").format((double)gyroscopeOrientation[1]))+ "\n\n"
-                + "Orientation Z (Yaw) :" + (new DecimalFormat("#.##").format((double)gyroscopeOrientation[2])));
-        */
+        Log.d("", "" + Math.abs(gyroscopeOrientation[0] - fantasma));
 
+        if(Math.abs(gyroscopeOrientation[0] - fantasma) < time_not_vib){
+            try {
+                MR.transmitMessage(GYROID, "vibratePlease");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            time_not_vib=0;
+        }
+        if (Math.abs(gyroscopeOrientation[0] - fantasma) < error) {
+
+            if (f == false) {
+                f = true;
+                t0=event.timestamp;
+            }
+            if(f==true && t0>0 && (event.timestamp-t0)*NS2S>2.5f){
+                Log.d("FANTASMA", "ENCONTRADO");
+            }
+
+
+        } else {
+            if (f == true) {
+                Log.d("FANTASMA", "FUERA");
+                f = false;
+                t0=-1;
+            }
+
+        }
+        if (!coords_set && normalized_gravity != null && normalized_gravity[2] > 0.9f) {
+            coords_set = true;
+            basevector = gyroscopeOrientation;
+        }
+        //Log.d("COORD", "S:"+coords_set + " V:" + basevector[0] + " " + basevector[1] + " " + basevector[2]);
+        //Log.d("COORD", "" + basevector2[0] + " " + basevector2[1] + " " + basevector2[2]);
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
