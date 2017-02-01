ac216e61c0a2a520d3c1b12f300a8778796e11f5
mikykeane <miguekeca@gmail.com>
2017-02-01 22:22:44 +0100

    solved xml issues

--- app/src/main/java/com/GhostlyRunes/StarPatternActivity.java
+++ app/src/main/java/com/GhostlyRunes/StarPatternActivity.java
@@ -10,16 +10,22 @@ public class StarPatternActivity extends MainActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_star_pattern);
+
         int[] pattern = new int[5];
         pattern[0]=R.id.Point1;
         pattern[1]=R.id.Point2;
         pattern[2]=R.id.Point3;
         pattern[3]=R.id.Point4;
         pattern[4]=R.id.Point5;
+
         View view = findViewById(R.id.activity_star_pattern);
+
         touch= new TouchHandler(this, TOUCHID);
+
         view.setOnTouchListener(touch);
+
         touch.setPattern(pattern);
         touch.startChecking();
     }
